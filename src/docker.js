e47363330333e95f08ba0e1cc05f2a3f4757f11a
Quan@MacbookPro2015 <quannq.it@gmail.com>
2020-02-11 15:01:38 +0700

    test

--- src/docker.js
+++ src/docker.js
@@ -57,7 +57,7 @@ const build = (imageName, buildArgs) => {
 
   core.info(`Building Docker image: ${imageName}`);
 
-  cp.execSync(createBuildCommand(dockerfile, imageName, buildArgs), { stdio: [process.stdout] });
+  cp.execSync(createBuildCommand(dockerfile, imageName, buildArgs), { stdio: 'inherit' });
 };
 
 const isEcr = registry => registry && registry.includes('amazonaws');
